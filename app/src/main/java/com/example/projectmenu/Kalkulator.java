44a5ff32e9b2bf7b30de537afd9955fad3a84a64
ASUS <albertramadan4@gmail.com>
2024-08-13 12:52:15 +0700

    Projek Menu option

---
+++ app/src/main/java/com/example/projectmenu/Kalkulator.java
@@ -0,0 +1,115 @@
+package com.example.projectmenu;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Kalkulator extends AppCompatActivity {
+    // Deklarasi Objek ke Variabel
+    EditText etangka1, etangka2;
+    Button btambah, bkurang, bkali, bbagi, bbersih;
+    TextView thasil;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_kalkulator);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        // Memanggil View di Layout dan Menerapkan di Variabel
+        // yang bertipe objek yang sama
+        etangka1 = findViewById(R.id.edangka1);
+        etangka2 = findViewById(R.id.edangka2);
+        btambah = findViewById(R.id.btntambah);
+        bkurang = findViewById(R.id.btnkurang);
+        bkali = findViewById(R.id.btnkali);
+        bbagi = findViewById(R.id.btnbagi);
+        bbersih = findViewById(R.id.btnbersih);
+        thasil = findViewById(R.id.txthasil);
+
+        btambah.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Memastikan bahwa kedua input tidak kosong
+                if ((etangka1.getText().length() > 0) && (etangka2.getText().length() > 0)) {
+                    double a1 = Double.parseDouble(etangka1.getText().toString());
+                    double a2 = Double.parseDouble(etangka2.getText().toString()); // Diperbaiki dari etangka1 ke etangka2
+                    double result = a1 + a2;
+                    thasil.setText(Double.toString(result));
+                } else {
+                    Toast.makeText(Kalkulator.this, "Angka1 dan Angka2 tidak boleh kosong", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        bkurang.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if ((etangka1.getText().length() > 0) && (etangka2.getText().length() > 0)) {
+                    double a1 = Double.parseDouble(etangka1.getText().toString());
+                    double a2 = Double.parseDouble(etangka2.getText().toString()); // Diperbaiki dari etangka1 ke etangka2
+                    double result = a1 - a2;
+                    thasil.setText(Double.toString(result));
+                } else {
+                    Toast.makeText(Kalkulator.this, "Angka1 dan Angka2 tidak boleh kosong", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        bkali.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if ((etangka1.getText().length() > 0) && (etangka2.getText().length() > 0)) {
+                    double a1 = Double.parseDouble(etangka1.getText().toString());
+                    double a2 = Double.parseDouble(etangka2.getText().toString()); // Diperbaiki dari etangka1 ke etangka2
+                    double result = a1 * a2;
+                    thasil.setText(Double.toString(result));
+                } else {
+                    Toast.makeText(Kalkulator.this, "Angka1 dan Angka2 tidak boleh kosong", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        bbagi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if ((etangka1.getText().length() > 0) && (etangka2.getText().length() > 0)) {
+                    double a1 = Double.parseDouble(etangka1.getText().toString());
+                    double a2 = Double.parseDouble(etangka2.getText().toString()); // Diperbaiki dari etangka1 ke etangka2
+                    if (a2 != 0) { // Cek pembagian dengan nol
+                        double result = a1 / a2;
+                        thasil.setText(Double.toString(result));
+                    } else {
+                        Toast.makeText(Kalkulator.this, "Pembagi tidak boleh nol", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(Kalkulator.this, "Angka1 dan Angka2 tidak boleh kosong", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        bbersih.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                etangka1.setText("");
+                etangka2.setText("");
+                thasil.setText("0");
+                etangka1.requestFocus();
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/projectmenu/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.projectmenu;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.optionmenu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId()==R.id.mnkalkulator){
+            startActivity(new Intent(this, Kalkulator.class));
+        } else if(item.getItemId()==R.id.mninformasi){
+            Intent intent = new Intent(this,Informasi.class);
+            startActivity(intent);
+        }
+        return true;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+    }
+}
